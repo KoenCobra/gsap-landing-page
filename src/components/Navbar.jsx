90a13c3b756479a4134bb7c790785847e658b46b
Koen De Groot <koendegroot84@gmail.com>
2025-10-21 15:55:08 +0200

    Refactor App component to include Navbar and remove placeholder content

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { navLinks } from "../../constants";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <a href="#home" className="flex items-center gap-2">
+          <p>Velvet Pour</p>
+        </a>
+
+        <ul>
+          {navLinks.map((item, index) => (
+            <li key={index}>
+              <a href={`#${item.id}`}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
