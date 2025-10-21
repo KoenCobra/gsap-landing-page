daa832e21d852eccc4f9f1edd1c1533b2165146e
Koen De Groot <koendegroot84@gmail.com>
2025-10-21 17:39:57 +0200

    Add GSAP scroll animations to Hero component for enhanced visual effects

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -30,6 +30,30 @@ const Hero = () => {
       stagger: 0.06,
       delay: 1,
     });
+
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: "#hero",
+          start: "top top",
+          end: "bottom top",
+          scrub: true,
+        },
+      })
+      .to(
+        ".left-leaf",
+        {
+          y: -200,
+        },
+        0
+      )
+      .to(
+        ".right-leaf",
+        {
+          y: 200,
+        },
+        0
+      );
   }, []);
 
   return (
